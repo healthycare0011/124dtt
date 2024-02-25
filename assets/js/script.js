b83c60077d84a01a8b5ddea614cc55da9536341e
healthycare0011 <156569341+healthycare0011@users.noreply.github.com>
2024-02-25 16:22:20 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,14 +16,7 @@ modalCloseBtn.addEventListener('click', modalCloseFunc);
 
 
 
-// notification toast variables
-const notificationToast = document.querySelector('[data-toast]');
-const toastCloseBtn = document.querySelector('[data-toast-close]');
 
-// notification toast eventListener
-toastCloseBtn.addEventListener('click', function () {
-  notificationToast.classList.add('closed');
-});
 
 
 
